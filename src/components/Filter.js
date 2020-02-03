ab4d308b6d7ebc5b466fa1e75875746a445ef8d2
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2020-02-04 00:47:02 +0100

    filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Input, InputGroupText, InputGroupAddon, InputGroup} from 'reactstrap'
 
-const Filter = () =>
+const Filter = ({filterFn}) =>
     <InputGroup>
         <InputGroupAddon addonType="prepend">
             <InputGroupText>Filter Term</InputGroupText>
         </InputGroupAddon>
-        <Input type="text" id="filter"  placeholder="enter filter term" />
+        <Input type="text" id="filter"  placeholder="enter filter term" onChange={filterFn}/>
     </InputGroup>
 
 
--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -5,14 +5,16 @@ import Filter from "./Filter";
 import Dialog from "./Dialog";
 import doFetch from "../util/fetchUtil";
 
-const headers = { headers: { 'Content-Type': 'application/json; charset=utf-8' } };
+const headers = {headers: {'Content-Type': 'application/json; charset=utf-8'}};
 
 const ToDoContainer = ({initialToDos, serverUrl}) => {
 
     const [toDos, setToDos] = useState([]);
+    const [copyOfTodos, setCopyOfTodos] = useState([]);
 
     const readAll = () => {
         doFetch({url: serverUrl, dataHandler: setToDos, errorText: "read todo's failed"});
+        setCopyOfTodos([...toDos]);
     };
 
     useEffect(readAll, []);
@@ -21,22 +23,47 @@ const ToDoContainer = ({initialToDos, serverUrl}) => {
 
         const dataHandler = savedTodo => setToDos([...toDos, savedTodo]);
 
-        doFetch({url: serverUrl, requestObject: {method: "POST", ...headers, body: JSON.stringify(toDo)}, dataHandler, errorText: "add todo failed"});
+        doFetch({
+            url: serverUrl,
+            requestObject: {method: "POST", ...headers, body: JSON.stringify(toDo)},
+            dataHandler,
+            errorText: "add todo failed"
+        });
     };
 
     const _delete = (id) => {
         const dataHandler = (ghj) => setToDos(toDos.filter(toDo => toDo.id !== id));
-        doFetch({url: `${serverUrl}/${id}`, requestObject: {method: "DELETE"}, dataHandler, errorText: "delete todo failed"});
+        doFetch({
+            url: `${serverUrl}/${id}`,
+            requestObject: {method: "DELETE"},
+            dataHandler,
+            errorText: "delete todo failed"
+        });
     };
 
     const update = (toUpdateToDo) => {
-       const dataHandler = updatedToDo => setToDos(toDos.map(toDo => toDo.id === updatedToDo.id ? updatedToDo : toDo));
-        doFetch({url: `${serverUrl}/${toUpdateToDo.id}`, requestObject: {method: "PUT", ...headers, body: JSON.stringify(toUpdateToDo)}, dataHandler, errorText: "update todo failed"});
+        const dataHandler = updatedToDo => setToDos(toDos.map(toDo => toDo.id === updatedToDo.id ? updatedToDo : toDo));
+        doFetch({
+            url: `${serverUrl}/${toUpdateToDo.id}`,
+            requestObject: {method: "PUT", ...headers, body: JSON.stringify(toUpdateToDo)},
+            dataHandler,
+            errorText: "update todo failed"
+        });
+    };
+
+    const filter = event => {
+        const data = [...copyOfTodos];
+        const filterTerm = event.target.value;
+        if (filterTerm === '') {
+            setToDos(data);
+        }
+        setToDos(data.filter(toDo => toDo.title.includes(filterTerm) || toDo.description.includes(filterTerm)));
     };
 
     return (<Container>
-        <Filter/>
-        <Dialog actionFn={add} toDo={{title: '', description: ''}} buttonStyle={{width: "100%", marginTop: "2%", marginBottom: "2%"}}
+        <Filter filterFn={filter}/>
+        <Dialog actionFn={add} toDo={{title: '', description: ''}}
+                buttonStyle={{width: "100%", marginTop: "2%", marginBottom: "2%"}}
                 headerText={"Add a new ToDo"} color={"success"} dialogButtonLabel={"add"} actionLabel={"create"}/>
         <Row>
             {toDos.map(toDo => <ToDoElement updateFn={update} deleteFn={_delete} key={toDo.id} toDo={toDo}/>)}
