8ff0b30c8b3b1a002c85ad453976a0994288c312
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2020-02-05 15:49:23 +0100

    created middleware to log the state in the store

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,18 @@ import ReduxThunk from 'redux-thunk'
 
 const consoleMessages = store => next => action => {
 
+    console.groupCollapsed(`Dispatching Action => ${action.type}`);
+    console.log(`old state in store: `);
+    console.dir(store.getState());
+
+    const result = next(action);
+
+    console.log(`new state in store: `);
+    console.dir(store.getState());
+    console.groupEnd();
+
+    return result;
 };
 
-export default initialState => createStore(reducer, initialState, applyMiddleware(ReduxThunk));
+export default initialState => createStore(reducer, initialState, applyMiddleware(ReduxThunk, consoleMessages));
 
