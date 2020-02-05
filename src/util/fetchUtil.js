8680acb5cf7526c81c43ca79095030f4c814a8e1
BenjaminBrodwolf <benjamin.brodwolf@students.fhnw.ch>
2020-02-05 14:15:23 +0100

    doFetch with dispatching the Action(Types)

--- src/util/fetchUtil.js
+++ src/util/fetchUtil.js
@@ -1,15 +1,16 @@
-const doFetch = async ({url, requestObject, dataHandler, errorText}) => {
-    try {
-        const response = await fetch(url, requestObject);
-        if (response.ok) {
-            const json = response.status !== 204 ? await response.json() : null;
-            dataHandler(json);
-        } else {
-            throw new Error(`${errorText}: ${response.status}`)
+const doFetch = ({url, requestObject, actionType, errorText}) =>
+    async dispatch => {
+        try {
+            const response = await fetch(url, requestObject);
+            if (response.ok) {
+                const json = response.status !== 204 ? await response.json() : null;
+                dispatch({type: actionType, data: json})
+            } else {
+                throw new Error(`${errorText}: ${response.status}`)
+            }
+        } catch (e) {
+            console.error(e);
         }
-    } catch (e) {
-        console.error(e);
-    }
-};
+    };
 
 export default doFetch;
